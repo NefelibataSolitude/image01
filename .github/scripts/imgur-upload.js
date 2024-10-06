52555957f0471f7392f7781b8b1830dc47731294
Tecoy <iTecoy@163.com>
2024-10-06 19:32:42 +0800

    action

--- .github/scripts/imgur-upload.js
+++ .github/scripts/imgur-upload.js
@@ -3,7 +3,6 @@ const { PicGo } = require("picgo");
 const path = require("path");
 // 获取 GitHub Actions 的工作目录
 const workspaceDir = process.env.GITHUB_WORKSPACE || process.cwd();
-console.log(workspaceDir);
 const imageExtensions = [
   ".jpg",
   ".jpeg",
@@ -29,13 +28,19 @@ const imageExtensions = [
       `git diff --name-status ${lastCommit} ${currentCommit}`,
       { encoding: "utf-8" }
     ).trim();
-    const images = changes.split("\n").filter((change) => {
-      const [status, filePath, ...other] = change.split("\t");
-      // 只处理修改的文件
-      if (
-        (status === "M" || status === "A") &&
-        imageExtensions.some((ext) => filePath.endsWith(ext))
-      ) {
+    const images = changes
+      .split("\n")
+      .filter((change) => {
+        const [status, filePath, ...other] = change.split("\t");
+        // 只保留修改 (M) 或添加 (A) 的文件，并且是图片文件
+        return (
+          (status === "M" || status === "A") &&
+          imageExtensions.some((ext) => filePath.endsWith(ext))
+        );
+      })
+      .map((change) => {
+        const [status, filePath, ...other] = change.split("\t");
+        // 处理文件路径
         if (/"/g.test(filePath) && /\\/.test(filePath)) {
           const cleanedfilePath = filePath.replace(/"/g, "");
           // 将转义字符转换为实际的字节
@@ -57,8 +62,8 @@ const imageExtensions = [
           return path.join(workspaceDir, buffer.toString());
         }
         return path.join(workspaceDir, filePath);
-      }
-    });
+      });
+
     if (images.length > 0) {
       // 执行上传操作
       const picgo = new PicGo();
